f35a9d6c7a4886192bf8b27122912655104efc00
Luca Cannarozzo <cannarozzoluca@live.it>
2022-03-03 10:26:04 +0100

    fix wrong globalStyleExport

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+import { GlobalStyle } from './stories/shared/globalStyle';
+
 //Components
 export * from './stories/button';
 
 
 // Theme settings
 export * from './stories/theme';
-export * from './stories/shared/globalStyle';
+export { GlobalStyle };
