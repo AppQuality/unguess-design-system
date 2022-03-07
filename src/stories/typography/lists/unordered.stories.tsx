081d1997a0ecedd3e155b6a6ec9c79cb5eb30a06
Luca Cannarozzo <cannarozzoluca@live.it>
2022-03-07 15:54:42 +0100

    feat(typography): add lists component with stories

---
+++ src/stories/typography/lists/unordered.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta, Story } from "@storybook/react";
+import { UnorderedList } from ".";
+import { UnorderedListArgs } from "./_types";
+
+const UnOrderedTemplate: Story<UnorderedListArgs> = ({ items, ...args }) => {
+  return (
+    <UnorderedList {...args}>
+      {items &&
+        items.map((item) => <UnorderedList.Item>{item}</UnorderedList.Item>)}
+    </UnorderedList>
+  );
+};
+
+const defaultArgs: UnorderedListArgs = {
+  items: ["Item 1", "Item 2", "Item 3"],
+};
+
+export const Unordered = UnOrderedTemplate.bind({});
+Unordered.args = defaultArgs as UnorderedListArgs;
+
+export default {
+  title: "Atoms/Typography/Lists",
+  component: UnorderedList,
+  argTypes: {
+    size: {
+      control: {
+        type: "select",
+        options: ["small", "medium", "large"],
+      },
+    },
+  },
+} as ComponentMeta<typeof UnorderedList>;
