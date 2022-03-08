c5871926c6beb7db9e3c4899ab8e34d638d53646
Marco Bonomo <marco.bonomo@app-quality.com>
2022-03-08 16:50:28 +0100

    feat(loaders): setup loaders structure

---
+++ src/stories/loaders/spinner/index.stories.tsx
@@ -0,0 +1,54 @@
+import { ComponentMeta, Story } from "@storybook/react";
+import { theme } from '../theme';
+import { Avatar } from ".";
+import { AvatarArgs } from "./_types";
+import { ReactComponent as UserIcon } from '@zendeskgarden/svg-icons/src/16/user-solo-stroke.svg';
+
+interface AvatarProps extends AvatarArgs {
+  type: 'icon' | 'image' | 'text';
+}
+
+const Template: Story<AvatarProps> = (args) => {
+  const children = (type: string) => {
+    if (type == "icon") return <UserIcon role="img" aria-label="User" />
+    if (type == "image") return <img alt="image avatar" src="https://s3.eu-west-1.amazonaws.com/appq.static/rossini.png" />
+    if (type == "text") return <Avatar.Text>LC</Avatar.Text>
+  }
+  return (
+    <Avatar {...args}>
+      {children(args.type)}
+    </Avatar>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  type: "text",
+  backgroundColor: theme.colors.primaryHue,
+};
+
+export const Square = Template.bind({});
+Square.args = {
+  type: "icon",
+  backgroundColor: theme.colors.primaryHue,
+  isSystem: true,
+  status: "available"
+};
+
+export default {
+  title: "Atoms/Avatar",
+  component: Avatar,
+  argTypes: {
+    badge: {
+      control: {
+        type: "text"
+      }
+    },
+    type: {
+      control: {
+        type: "select",
+        options: ['icon', 'image', 'text']
+      }
+    }
+  }
+} as ComponentMeta<typeof Avatar>;
