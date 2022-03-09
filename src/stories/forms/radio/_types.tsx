691a4a53392ffc3a48fccf86a003ead6ca69f9c6
Marco Bonomo <marco.bonomo@app-quality.com>
2022-03-09 17:34:54 +0100

    feat(radio): add radio component

---
+++ src/stories/forms/radio/_types.tsx
@@ -0,0 +1,5 @@
+import { IRadioProps } from "@zendeskgarden/react-forms";
+
+export interface RadioArgs extends IRadioProps {
+  
+}
---
+++ src/stories/forms/radio/index.stories.tsx
@@ -0,0 +1,36 @@
+import { ComponentMeta, Story } from "@storybook/react";
+import { Radio } from ".";
+import { Field } from "../../field";
+import { Label } from "../../label";
+import { Row } from "../../grid/row";
+import { Col } from "../../grid/col";
+import { RadioArgs } from "./_types";
+
+const Template: Story<RadioArgs> = (args) => {
+  return (
+    <Row>
+      <Col>
+        <Field>
+          <Radio {...args}>
+            <Label isRegular={true}>Questa è la Label del Radio</Label>
+          </Radio>
+        </Field>
+      </Col>
+    </Row>
+  )
+}
+
+export const Default = Template.bind({});
+Default.args = {
+  checked: false,
+};
+
+export const Checked = Template.bind({});
+Checked.args = {
+  checked: true,
+};
+
+export default {
+  title: "Atoms/Forms/Radio",
+  component: Radio
+} as ComponentMeta<typeof Radio>;
