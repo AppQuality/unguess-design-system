ed9851b6ecd34e60114f25939fcb6b8cc760df0b
Luca Cannarozzo <cannarozzoluca@live.it>
2022-03-11 13:14:24 +0100

    fix(field): fix wrong import for form field component

---
+++ src/stories/forms/field/index.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentMeta, Story } from "@storybook/react";
+import { Field } from ".";
+import { FieldArgs } from "./_types";
+
+const Template: Story<FieldArgs> = (args) => {
+  return (
+    <Field {...args}>
+      <textarea value="Il componente Field contiene questa textarea" />
+    </Field>
+  )
+}
+
+export const Default = Template.bind({});
+Default.args = {
+  
+};
+
+export default {
+  title: "Atoms/Field",
+  component: Field,
+} as ComponentMeta<typeof Field>;
