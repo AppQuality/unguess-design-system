220b7347a4d585a837c5de7f73269c5a9972aa5a
Marco Bonomo <marco.bonomo@app-quality.com>
2022-03-14 10:20:21 +0100

    feat(export): export all compontents and deleted unused ones

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,77 @@
 import { GlobalStyle } from './stories/shared/globalStyle';
 
-//-------------- Components --------------
+// -------------- Components --------------
+
+// --- Accordions ---
+export * from './stories/accordions';
 
 // --- Avatar ---
 export * from './stories/avatar';
-
 
 // --- Breadcrumbs ---
 export * from './stories/breadcrumbs';
 
 // --- Buttons ---
-export * from './stories/buttons/button';
 export * from './stories/buttons/anchor';
+export * from './stories/buttons/button';
 export * from './stories/buttons/button-group';
 export * from './stories/buttons/icon-button';
 export * from './stories/buttons/split-button';
 
+// --- Cards ---
+export * from './stories/cards';
+
+// --- Close ---
+export * from './stories/close';
+
+// --- Dropdowns ---
+export * as DropdownField from './stories/dropdowns/field';
+export * from './stories/dropdowns/item';
+export * from './stories/dropdowns/menu';
+export * from './stories/dropdowns/multiselect';
+export * from './stories/dropdowns/select';
+
+// --- Forms ---
+export * from './stories/forms/checkbox';
+export * as FormField from './stories/forms/field';
+export * from './stories/forms/input';
+export * from './stories/forms/radio';
+export * from './stories/forms/textarea';
+export * from './stories/forms/toggle';
+
+// --- Grid ---
+export * from './stories/grid/col';
+export * from './stories/grid/row';
+export * from './stories/grid/grid';
+
+// --- Label ---
+export * from './stories/label';
+
 // --- Loaders ---
 export * from './stories/loaders/spinner';
 
+// --- Login Form ---
+export * from './stories/login-form';
+
+// --- Notifications ---
+export * from './stories/notifications';
+
 // --- Theme ---
 export * from './stories/theme';
+
+// --- Title ---
+export * from './stories/title';
+
+// --- Trigger ---
+export * from './stories/trigger';
+
+// --- Typography ---
+export * from './stories/typography/block-quote';
+export * from './stories/typography/code';
+export * from './stories/typography/ellipsis';
+export * from './stories/typography/lists';
+export * from './stories/typography/paragraph';
+export * from './stories/typography/span';
+export * from './stories/typography/typescale';
+
 export { GlobalStyle };
--- src/stories/item/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Item as ZendeskItem } from "@zendeskgarden/react-dropdowns";
-import { ItemArgs } from "./_types";
-import styled from "styled-components";
-
-const UgItem = styled(ZendeskItem)``;
-
-const Item = (props: ItemArgs) => <UgItem {...props} />;
-
-export { Item };
