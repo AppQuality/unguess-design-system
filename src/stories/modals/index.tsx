582a18b094b9ffc53d3d9787e4fcd19b2d981259
Luca Cannarozzo <cannarozzoluca@live.it>
2022-03-24 13:25:17 +0100

    build(components): add component to index

---
+++ src/stories/modals/index.tsx
@@ -0,0 +1,21 @@
+import {
+  Modal as ZendeskModal,
+  Header as ModalHeader,
+  Body as ModalBody,
+  Footer as ModalFooter,
+  FooterItem as ZendeskFooterItem,
+} from "@zendeskgarden/react-modals";
+import { ModalArgs } from "./_types";
+import styled from "styled-components";
+
+const ugModalBody = styled(ModalBody)`
+  color: ${({ theme }) => theme.palette.grey["800"]};
+`;
+
+const Modal = (props: ModalArgs) => <ZendeskModal {...props} />;
+const FooterItem = ZendeskFooterItem;
+Modal.Header = ModalHeader;
+Modal.Body = ugModalBody;
+Modal.Footer = ModalFooter;
+
+export { Modal, FooterItem };
--- src/stories/navigation/nav/nav-item/index.ts
+++ src/stories/navigation/nav/nav-item/index.ts
@@ -2,5 +2,7 @@ import { NavItem } from "./navItem";
 import { NavItemIcon } from "./navItemIcon";
 import { NavItemText } from "./navItemText";
 import { NavToggle } from "./navToggle";
+import { NavDivider } from "./navDivider";
+import { NavItemProject } from "./navItemProject";
 
-export { NavItem, NavItemIcon, NavItemText, NavToggle };
+export { NavItem, NavItemIcon, NavItemText, NavToggle, NavDivider, NavItemProject };
