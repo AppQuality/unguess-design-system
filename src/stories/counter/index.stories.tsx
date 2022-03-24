f9692f88c4066719e27345defcb418b27e865f5e
Luca Cannarozzo <cannarozzoluca@live.it>
2022-03-24 15:33:40 +0100

    feat(counter): add Counter component

---
+++ src/stories/counter/index.stories.tsx
@@ -0,0 +1,42 @@
+import { ComponentMeta, Story } from "@storybook/react";
+import { Counter } from ".";
+import { Col } from "../grid/col";
+import { Row } from "../grid/row";
+import { CounterArgs } from "./_types";
+
+interface TagStoryProps extends CounterArgs {
+  hasAvatar: boolean;
+}
+
+const Template: Story<TagStoryProps> = ({hasAvatar, ...args}) => (
+  <Row>
+    <Col textAlign="center">
+      <Counter {...args} />
+    </Col>
+  </Row>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Completed",
+  counter: 1,
+  status: "completed",
+  hasAvatar: true
+};
+
+// export const Avatar = Template.bind({});
+// Avatar.args = {
+//   title: "Tag con avatar",
+//   hasAvatar: true
+// };
+
+// export const Close = Template.bind({});
+// Close.args = {
+//   title: "Tag rimovibile",
+//   canBeClosed: true
+// };
+
+export default {
+  title: "Atoms/Counter",
+  component: Counter,
+} as ComponentMeta<typeof Counter>;
