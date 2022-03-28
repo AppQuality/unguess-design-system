59d71360cabb3aa7de835b69bf3b6e9e4f8b0ef3
Luca Cannarozzo <cannarozzoluca@live.it>
2022-03-28 10:08:09 +0200

    fix(main): add spaces as described by dressBerry

--- src/stories/navigation/main/index.tsx
+++ src/stories/navigation/main/index.tsx
@@ -1,9 +1,16 @@
 import { Main as ZendeskMain } from "@zendeskgarden/react-chrome";
 import { MainArgs } from "./_types";
+import styled from "styled-components";
+
+const UgMain = styled(ZendeskMain)`
+  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    margin: 0 ${({ theme }) => theme.space.xxl }
+  }
+`;
 
 /**
  * A Main defines the main content of an HTML document which displays on the browser
  */
-const Main = (props: MainArgs) => <ZendeskMain {...props} />;
+const Main = (props: MainArgs) => <UgMain {...props} />;
 
 export { Main };
