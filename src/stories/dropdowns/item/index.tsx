dc18af34d2a56ad019c52c971b54055ef05c1960
Luca Cannarozzo <cannarozzoluca@live.it>
2022-04-13 17:03:43 +0200

    fix(menuitem): force hover background color

--- src/stories/dropdowns/item/index.tsx
+++ src/stories/dropdowns/item/index.tsx
@@ -2,7 +2,11 @@ import { Item as ZendeskItem } from "@zendeskgarden/react-dropdowns";
 import { ItemArgs } from "./_types";
 import styled from "styled-components";
 
-const UgItem = styled(ZendeskItem)``;
+const UgItem = styled(ZendeskItem)`
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.blue[100]};
+  }
+`;
 
 const Item = (props: ItemArgs) => <UgItem {...props} />;
 
