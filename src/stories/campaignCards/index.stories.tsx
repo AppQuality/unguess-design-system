f84d1fedd6a4b9030b11ad39cb9933e7d628c523
Marco Bonomo <marco.bonomo@app-quality.com>
2022-04-15 13:03:28 +0200

    fix(campaign-cards): add tag max-width

--- src/stories/campaignCards/index.stories.tsx
+++ src/stories/campaignCards/index.stories.tsx
@@ -13,20 +13,30 @@ const defaultArgs: CampaignCardsProps = {
   isNew: true,
   date: "24/04/2022",
   title: "title html",
-  projectTitle:  "This is the project title",
+  projectTitle: "This is the project title",
   campaignTitle: "This is the campaign title",
   status: 'COMPLETED',
   type: 'EXPERIENTIAL',
-  pillText: 'Experiential Test'
+  pillText: 'Experiential Testone Lungone Furbone'
 }
 
 const MultiTemplate: Story<CampaignCardsProps> = (args) => {
-  return <Row>
-    <Col><CampaignCard {...args} projectTitle={"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"} campaignTitle={"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"} type={"FUNCTIONAL"} date={"20/10/2022"} status={"INCOMING"} /></Col>
-    <Col><CampaignCard {...args} isNew={false} status={"PROGRESS"} /></Col>
-    <Col><CampaignCard {...args} isNew={false} type={"FUNCTIONAL"} pillText={"Funcional Test"} /></Col>
-    <Col><CampaignCard {...args} isNew={false} /></Col>
-  </Row>
+  return (
+    <Row>
+      <Col size={3}>
+        <CampaignCard {...args} projectTitle={"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"} campaignTitle={"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"} type={"FUNCTIONAL"} date={"20/10/2022"} status={"INCOMING"} />
+      </Col>
+      <Col size={3}>
+        <CampaignCard {...args} isNew={false} status={"PROGRESS"} />
+      </Col>
+      <Col size={3}>+
+        <CampaignCard {...args} isNew={false} type={"FUNCTIONAL"} pillText={"Funcional Test"} />
+      </Col>
+      <Col size={3}>
+        <CampaignCard {...args} isNew={false} />
+      </Col>
+    </Row>
+  )
 }
 
 const SingleTemplate: Story<CampaignCardsProps> = (args) => {
