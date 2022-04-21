c49fc1b334a2efa17a4f052dc9ddef7074e716a3
cicababba <cicababba1@gmail.com>
2022-04-21 16:54:11 +0200

    style(scrollbar): styled scrollbar based on CUP-627

--- src/stories/shared/globalStyle.ts
+++ src/stories/shared/globalStyle.ts
@@ -2,6 +2,33 @@ import { createGlobalStyle } from "styled-components";
 import { theme } from "../theme";
 
 const GlobalStyle = createGlobalStyle`
+
+  ::-webkit-scrollbar-track:hover {
+    /*border: solid 1px #E9EBED;*/
+    border-left: solid 1px #E9EBED;
+    border-right: solid 1px #E9EBED;
+  }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    border: solid 2px transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    box-shadow: inset 0 0 10px 10px #B0C2CB;
+    border: solid 2px transparent;
+    border-radius: 25px;
+  }
+
+  /* This stuff is for Firefox */
+  * {
+    scrollbar-color: #B0C2CB #FFFFFF;
+    scrollbar-width: thin;
+  }
+  
   body {
     font-family: ${theme.fonts.system};
     -webkit-font-smoothing: antialiased;
@@ -11,7 +38,7 @@ const GlobalStyle = createGlobalStyle`
   svg {
     max-height: 100%;
   }
-  
-  `;
+
+`;
 
 export { GlobalStyle };
