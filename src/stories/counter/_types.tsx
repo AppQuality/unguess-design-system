7b088efc3d51affb6940d61bd96b78778d0b3cfe
Luca Cannarozzo <cannarozzoluca@live.it>
2022-04-26 11:38:54 +0200

    feat(counter): remove counter required props

--- src/stories/counter/_types.tsx
+++ src/stories/counter/_types.tsx
@@ -4,5 +4,5 @@ import { TagArgs } from "../tags/_types";
 
 export interface CounterArgs extends TagArgs {
   status: "progress" | "completed" | "incoming" | "functional" | "experiential";
-  counter: number;
+  counter?: number;
 }
