e04b86edb3ff6fe1cc62845c520e5e613018f0b3
Luca Cannarozzo <cannarozzoluca@live.it>
2022-05-05 18:33:52 +0200

    feat(hint): add hint component

--- src/stories/forms/input/index.tsx
+++ src/stories/forms/input/index.tsx
@@ -1,8 +1,9 @@
-import { Input as ZendeskInput } from "@zendeskgarden/react-forms";
+import { Input as ZendeskInput, Hint as ZendeskHint } from "@zendeskgarden/react-forms";
 import { InputArgs } from "./_types";
 import styled from "styled-components";
 
 const UgInput = styled(ZendeskInput)``;
+const Hint = styled(ZendeskHint)``;
 
 /**
    * An Input lets users enter text into a field.
@@ -13,4 +14,7 @@ const UgInput = styled(ZendeskInput)``;
    **/
 const Input = (props: InputArgs) => <UgInput {...props} />;
 
-export { Input };
+
+
+
+export { Input, Hint };
