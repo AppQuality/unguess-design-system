8bbf1af86426d0f1c66e9c2169294749d78c0e17
Luca Cannarozzo <cannarozzoluca@live.it>
2022-06-03 19:34:45 +0200

    wip(info-card): replace components with SpecialCard

--- src/stories/info-cards/index.tsx
+++ src/stories/info-cards/index.tsx
@@ -4,124 +4,117 @@ import { cardStyle } from "../theme/mixins";
 import { InfoCardProps } from "./_types";
 import styled from "styled-components";
 import { LG, SM } from "../typography/typescale";
+import { SpecialCard } from "../special-cards";
 
 const StyledCard = styled(Card)`
-    ${cardStyle}
+  ${cardStyle}
 `;
 
 const CardContent = styled.div`
-    z-index: 1;
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    justify-content: flex-start;
-    flex-grow: 1;
+  z-index: 1;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+  flex-grow: 1;
 `;
 
 const FooterContent = styled.div`
-    padding-top: ${({ theme }) => theme.space.sm};
-    margin-top: auto;
-    width: 100%;
+  padding-top: ${({ theme }) => theme.space.sm};
+  margin-top: auto;
+  width: 100%;
 `;
 
 const ButtonsWrap = styled.div`
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    justify-content: flex-start;
-    width: 100%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
 
-    button {
-        margin-top: ${({ theme }) => theme.space.sm};
-    }
+  button {
+    padding: ${({ theme }) => theme.space.xxs} 0;
+  }
+
+  button:not(:first-child) {
+    margin-top: ${({ theme }) => theme.space.xs};
+  }
 `;
 
 const CardHeader = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    justify-content: flex-start;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: flex-start;
 `;
 
 const ImgContainer = styled.div`
-    width: 100%;
+  width: 100%;
 
-    svg, img {
-        width: 100%;
-        max-width: 100%;
-    }
+  svg,
+  img {
+    width: 100%;
+    max-width: 100%;
+  }
 `;
 
 const CardBody = styled.div`
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    justify-content: flex-start;
-    margin-top: ${({ theme }) => theme.space.sm};
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-top: ${({ theme }) => theme.space.sm};
 `;
 
 const CardFooter = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    justify-content: flex-start;
-    margin-top: ${({ theme }) => theme.space.sm};
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: flex-start;
+  margin-top: ${({ theme }) => theme.space.sm};
 `;
 
 const Divider = styled.div`
-    width: 100%;
-    height: 1px;
-    margin: ${({ theme }) => theme.space.base}px 0;
-    background-color: ${({ theme }) => theme.palette.grey["300"]};
+  width: 100%;
+  height: 1px;
+  margin: ${({ theme }) => theme.space.base}px 0;
+  background-color: ${({ theme }) => theme.palette.grey["300"]};
 `;
 
 const InfoSubtitle = styled(SM)`
-    color: ${({ theme }) => theme.palette.grey[500]};
-    margin-bottom: ${({ theme }) => theme.space.base}px;
+  color: ${({ theme }) => theme.palette.grey[500]};
+  margin-bottom: ${({ theme }) => theme.space.base}px;
 `;
 
 const InfoTitle = styled(LG)`
-    color: ${({ theme }) => theme.palette.blue[600]};
-    margin-bottom: ${({ theme }) => theme.space.base}px;
-    font-weight: ${({ theme }) => theme.fontWeights.semibold};
+  color: ${({ theme }) => theme.palette.blue[600]};
+  margin-bottom: ${({ theme }) => theme.space.base}px;
+  font-weight: ${({ theme }) => theme.fontWeights.semibold};
 `;
 
 const InfoCard = (props: InfoCardProps) => {
-    return (
-        <StyledCard {...props}>
-            <CardContent>
-                <>
-                    {props.infoImg && (
-                        <CardHeader>
-                            <ImgContainer>{props.infoImg}</ImgContainer>
-                        </CardHeader>
-                    )}
-                    <CardBody>
-                        {props.infoSubtitle && (
-                            <InfoSubtitle>{props.infoSubtitle}</InfoSubtitle>
-                        )}
-                        {props.infoTitle && (
-                            <InfoTitle>{props.infoTitle}</InfoTitle>
-                        )}
-                    </CardBody>
-                </>
-                {props.infoButtons && (
-                    <FooterContent>
-                        <Divider />
-                        <CardFooter>
-                            {props.infoButtons && (
-                                <ButtonsWrap>
-                                    {props.infoButtons.map((button, index) => (
-                                        button
-                                    ))}
-                                </ButtonsWrap>
-                            )}
-                        </CardFooter>
-                    </FooterContent>
-                )}
-            </CardContent>
-        </StyledCard>
-    )
+  return (
+    <SpecialCard {...props}>
+      {props.infoImg && <SpecialCard.Thumb>{props.infoImg}</SpecialCard.Thumb>}
+      <SpecialCard.Header>
+        {props.infoSubtitle && (
+          <SpecialCard.Header.Label>
+            {props.infoSubtitle}
+          </SpecialCard.Header.Label>
+        )}
+        {props.infoTitle && (
+          <SpecialCard.Header.Title>{props.infoTitle}</SpecialCard.Header.Title>
+        )}
+      </SpecialCard.Header>
+      {props.infoButtons && (
+        <SpecialCard.Footer direction="column" justifyContent="center">
+          <ButtonsWrap>
+            {props.infoButtons.map((button, index) => button)}
+          </ButtonsWrap>
+        </SpecialCard.Footer>
+      )}
+    </SpecialCard>
+  );
 };
 
 export { InfoCard };
