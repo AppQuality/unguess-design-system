315fca00c2f600b9ea7226fba1bd131086ebed1d
Luca Cannarozzo <cannarozzoluca@live.it>
2022-06-03 19:06:10 +0200

    refactor(campaign-card): replace components with new special card

--- src/stories/campaign-cards/index.tsx
+++ src/stories/campaign-cards/index.tsx
@@ -11,6 +11,9 @@ import { ReactComponent as ProgressIcon } from "../../assets/icons/campaign-prog
 import { ReactComponent as IncomingIcon } from "../../assets/icons/campaign-incoming.svg";
 import { CampaignCardSkeleton } from "./skeleton";
 import { cardStyle } from "../theme/mixins";
+import { SpecialCard } from "../special-cards";
+import { SM } from "../typography/typescale";
+import { Ellipsis } from "../typography/ellipsis";
 
 const getStatusIcon = (status: string) => {
   switch (status) {
@@ -36,10 +39,6 @@ const getTypeDataIcon = (type?: string) => {
   }
 };
 
-export const Wrapper = styled(Card)`
-  ${cardStyle}
-`;
-
 const StyledTag = styled(Tag)`
   color: ${({ theme }) => theme.palette.grey["700"]};
   max-width: 75%;
@@ -53,73 +52,29 @@ const StyledTagNew = styled(Tag)`
   color: ${({ theme }) => theme.palette.white};
 `;
 
-const StyledLabel = styled(Label)`
-  font-size: ${({ theme }) => theme.fontSizes.sm};
+const StyledLabel = styled(SM)`
   color: ${({ theme }) => theme.palette.grey["500"]};
-  cursor: pointer;
 `;
 
-const StyledTitleLabel = styled(Label)`
-  color: ${({ theme }) => theme.palette.blue["600"]};
-  font-size: ${({ theme }) => theme.fontSizes.lg};
-  word-wrap: break-word;
-  cursor: pointer;
-`;
-
-export const Divider = styled.div`
-  width: 100%;
-  height: 1px;
-  margin: ${({ theme }) => theme.space.lg} 0;
-  background-color: ${({ theme }) => theme.palette.grey["300"]};
-`;
-
-export const CardHeader = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-  height: ${({ theme }) => theme.space.base * 6}px;
-`;
-
-export const CardBody = styled.div`
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  justify-content: flex-start;
-  margin-top: ${({ theme }) => theme.space.xxl};
-  flex: 1;
-`;
-
-export const CardFooter = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const CampaignCard = (props: CampaignCardsProps) => {
-  const { isNew, date, projectTitle, campaignTitle, status, type, labelNew } =
-    props;
-
+const CampaignCard = ({
+  isNew,
+  date,
+  projectTitle,
+  campaignTitle,
+  status,
+  type,
+  labelNew,
+  ...props
+}: CampaignCardsProps) => {
   const StatusIcon = getStatusIcon(status ?? "PROGRESS");
   const PillIcon = getTypeDataIcon(type);
-
-  let projectTitleCut = projectTitle;
-  if (projectTitle.length > 42) {
-    projectTitleCut = `${projectTitle.substring(0, 39)}...`;
-  }
-
-  let campaignTitleCut = campaignTitle;
-  if (campaignTitle.length > 29) {
-    campaignTitleCut = `${campaignTitle.substring(0, 26)}...`;
-  }
 
   return props.isLoading ? (
     <CampaignCardSkeleton />
   ) : (
-    <Wrapper {...props}>
-      <CardHeader>
-        <StyledLabel isRegular>{date}</StyledLabel>
+    <SpecialCard {...props}>
+      <SpecialCard.Meta>
+        <StyledLabel>{date}</StyledLabel>
         {isNew && (
           <StyledTagNew
             hue={theme.palette.fuschia["600"]}
@@ -130,13 +85,16 @@ const CampaignCard = (props: CampaignCardsProps) => {
             {labelNew ? labelNew : "New!"}
           </StyledTagNew>
         )}
-      </CardHeader>
-      <CardBody>
-        <StyledLabel isRegular>{projectTitleCut}</StyledLabel>
-        <StyledTitleLabel isRegular>{campaignTitleCut}</StyledTitleLabel>
-      </CardBody>
-      <Divider />
-      <CardFooter>
+      </SpecialCard.Meta>
+      <SpecialCard.Header>
+        <SpecialCard.Header.Label>
+          <Ellipsis style={{ width: "200px" }}>{projectTitle}</Ellipsis>
+        </SpecialCard.Header.Label>
+        <SpecialCard.Header.Title>
+          <Ellipsis style={{ width: "200px" }}>{campaignTitle}</Ellipsis>
+        </SpecialCard.Header.Title>
+      </SpecialCard.Header>
+      <SpecialCard.Footer>
         {props.pillText && (
           <StyledTag
             size="large"
@@ -151,8 +109,8 @@ const CampaignCard = (props: CampaignCardsProps) => {
           </StyledTag>
         )}
         <StatusIcon />
-      </CardFooter>
-    </Wrapper>
+      </SpecialCard.Footer>
+    </SpecialCard>
   );
 };
 
