3517a60a08fc80c6198bd525bb5b4871cd761a9d
Luca Cannarozzo <cannarozzoluca@live.it>
2022-06-03 17:25:58 +0200

    feat(card): add meta subcomponent

---
+++ src/stories/special-cards/styled/meta.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { CardMetaProps } from "../_types";
+
+export const CardMeta = styled.div<CardMetaProps>`
+  ${({ theme, direction, justifyContent }) => `
+        display: flex;
+        align-items: center;
+        flex-direction: ${direction || "row"};
+        justify-content: ${justifyContent || "space-between"};
+        height: ${theme.space.base * 6}px;
+        padding: ${theme.space.xxs} 0;
+        margin-bottom: ${theme.space.xs};
+    `}
+    
+`;
