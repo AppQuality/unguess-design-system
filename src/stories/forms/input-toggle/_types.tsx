5d28a67e78d6d2ddfa3a63064225519f54c3ab64
Marco Bonomo <marco.bonomo@unguess.io>
2022-09-07 15:19:42 +0200

    fix(input-toggle): remove useless focusInset props + fix size undefined

--- src/stories/forms/input-toggle/_types.tsx
+++ src/stories/forms/input-toggle/_types.tsx
@@ -3,12 +3,10 @@ import { IInputProps } from "@zendeskgarden/react-forms";
 export interface InputToggleArgs extends IInputProps {
   /** Removes borders and padding */
   isBare?: boolean;
-  /** Applies inset `box-shadow` styling on focus */
-  focusInset?: boolean;
   /** Applies validation state styling */
   validation?: 'success' | 'warning' | 'error';
   /** Sets input size */
-  size?: number | undefined;
+  size?: number;
   /** Sets the input label */
   label?: string;
   /** Sets the input message */
