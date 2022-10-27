007cbac72fa13924466ee983d8ff3ab4569989c7
Marco Bonomo <marco.bonomo@unguess.io>
2022-10-27 12:53:40 +0200

    feat(charts): add color scheme support for components

--- src/stories/charts/pie/index.tsx
+++ src/stories/charts/pie/index.tsx
@@ -1,15 +1,19 @@
 import { ResponsivePie } from "@nivo/pie";
-import { DEFAULT_CHARTS_THEME } from "../../theme/charts";
+import {
+  DEFAULT_CHARTS_COLOR_SCHEME,
+  DEFAULT_CHARTS_THEME,
+} from "../../theme/charts";
 import { PieChartProps } from "./_types";
 import { ChartContainer } from "../ChartContainer";
 
-const PieChart = ({ theme, ...props }: PieChartProps) => (
+const PieChart = ({ theme, colors, ...props }: PieChartProps) => (
   <ChartContainer width={props.width} height={props.height}>
     <ResponsivePie
       theme={{
         ...DEFAULT_CHARTS_THEME,
         ...theme,
       }}
+      colors={colors ?? DEFAULT_CHARTS_COLOR_SCHEME}
       {...props}
     />
   </ChartContainer>
--- src/stories/charts/stream/index.tsx
+++ src/stories/charts/stream/index.tsx
@@ -1,18 +1,22 @@
 import { ResponsiveStream } from "@nivo/stream";
 import { StreamChartProps } from "./_types";
-import { DEFAULT_CHARTS_THEME } from "../../theme/charts";
+import {
+  DEFAULT_CHARTS_COLOR_SCHEME,
+  DEFAULT_CHARTS_THEME,
+} from "../../theme/charts";
 import { ChartContainer } from "../ChartContainer";
 
-const StreamChart = ({ theme, ...props }: StreamChartProps) => (
-    <ChartContainer width={props.width} height={props.height}>
-        <ResponsiveStream
-            theme={{
-                ...DEFAULT_CHARTS_THEME,
-                ...theme,
-            }}
-            {...props}
-        />
-    </ChartContainer>
+const StreamChart = ({ theme, colors, ...props }: StreamChartProps) => (
+  <ChartContainer width={props.width} height={props.height}>
+    <ResponsiveStream
+      theme={{
+        ...DEFAULT_CHARTS_THEME,
+        ...theme,
+      }}
+      colors={colors ?? DEFAULT_CHARTS_COLOR_SCHEME}
+      {...props}
+    />
+  </ChartContainer>
 );
 
 export { StreamChart };
