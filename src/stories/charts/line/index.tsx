729bfb8e71760b878d0211bc4d3e11a030ccdbde
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-27 19:38:35 +0200

    feat: add line chart [skip ci]

---
+++ src/stories/charts/line/index.tsx
@@ -0,0 +1,23 @@
+import { ResponsiveLine } from "@nivo/line";
+import {
+  DEFAULT_CHARTS_COLOR_SCHEME,
+  DEFAULT_CHARTS_THEME,
+} from "../../theme/charts";
+import { LineChartProps } from "./_types";
+import { ChartContainer } from "../ChartContainer";
+
+const LineChart = ({ theme, colors, width, height, ...props }: LineChartProps) => (
+  <ChartContainer width={width} height={height}>
+    {/* @ts-ignore */}
+    <ResponsiveLine
+      theme={{
+        ...DEFAULT_CHARTS_THEME,
+        ...theme,
+      }}
+      colors={colors ?? DEFAULT_CHARTS_COLOR_SCHEME}
+      {...props}
+    />
+  </ChartContainer>
+);
+
+export { LineChart };
