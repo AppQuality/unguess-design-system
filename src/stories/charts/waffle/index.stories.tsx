43cfddd521456a210b7e62ec631147ebce0e0307
Marco Bonomo <marco.bonomo@unguess.io>
2022-11-15 13:41:57 +0100

    refactor(waffle): refactor component + simplified props

--- src/stories/charts/waffle/index.stories.tsx
+++ src/stories/charts/waffle/index.stories.tsx
@@ -1,8 +1,6 @@
 import { ComponentMeta, Story } from "@storybook/react";
-import { theme } from "../../theme";
 import { WaffleChart } from ".";
 import { WaffleChartProps } from "./_types";
-import { CHARTS_COLOR_SCHEME_MONO } from "../../theme/charts";
 
 const Template: Story<WaffleChartProps> = (args) => <WaffleChart {...args} />;
 export const Default = Template.bind({});
@@ -11,31 +9,17 @@ Default.args = {
   height: "90vh",
   data: [
     {
-      "id": "men",
-      "label": "men",
-      "value": 46,
+      label: "Unique bugs",
+      value: 30,
     },
     {
-      "id": "women",
-      "label": "women",
-      "value": 54,
+      label: "Total bugs",
+      value: 100,
     },
   ],
-  colors: CHARTS_COLOR_SCHEME_MONO,
-  total:100,
-  rows:18,
-  columns:14
 };
 
 export default {
   title: "Atoms/Charts/Waffle",
   component: WaffleChart,
-  argTypes: {
-    theme: {
-      control: {
-        type: "select",
-        options: Object.keys(theme),
-      },
-    },
-  },
 } as ComponentMeta<typeof WaffleChart>;
