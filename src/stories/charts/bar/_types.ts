63a54abc38ff719f70d0dc86d39fd0d5d76a27ad
Davide Bizzi <davide.bizzi@app-quality.com>
2022-11-28 12:29:52 +0100

    chore: Remove unused components

--- src/stories/charts/bar/_types.ts
+++
@@ -1,7 +0,0 @@
-import { BarDatum, BarSvgProps } from "@nivo/bar";
-
-export interface BarChartProps extends Omit<BarSvgProps<BarDatum>, "height" | "width"> {
-    data: BarDatum[];
-    width?: string;
-    height?: string;
-}
--- src/stories/charts/stream/_types.ts
+++
@@ -1,7 +0,0 @@
-import { StreamDatum, StreamSvgProps } from "@nivo/stream";
-
-export interface StreamChartProps extends Omit<StreamSvgProps<StreamDatum>, "width" | "height"> {
-    data: StreamDatum[];
-    width?: string;
-    height?: string;
-}
