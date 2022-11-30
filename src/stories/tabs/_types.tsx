930f451b28696cbe69415ab3e3c64df6e4bdabcd
Luca Cannarozzo <cannarozzoluca@live.it>
2022-11-30 15:52:43 +0100

    feat(tabs): replace zendesk tabs with handmade ones

--- src/stories/tabs/_types.tsx
+++ src/stories/tabs/_types.tsx
@@ -1,34 +1,21 @@
 import { ITabsProps } from "@zendeskgarden/react-tabs";
 
 export interface TabsArgs extends ITabsProps {
-  /**
-   * Arranges the tabs vertically
-   */
-  isVertical?: boolean;
-  /**
-   * Specifies the currently selected tab
-   */
-  selectedItem?: string;
-  /**
-   * Handles tab selection
-   *
-   * @param {String} selectedItem The selected tab's `item` value
-   */
-}
-
-export interface TabsContextType {
-  /**
-   * Specifies the currently selected tab
-   */
-  selectedItem?: string;
-  /**
-   * Handles tab selection
-   *
-   * @param {String} selectedItem The selected tab's `item` value
-   */
-  setSelectedItem?: (item: string) => void;
-}
-
-export interface CustomTabsArgs extends ITabsProps {
   selectedIndex?: number;
+
+  onTabChange?: (index: number) => void;
+}
+
+export interface TabItemProps {
+  children: React.ReactNode;
+  index: number;
+  isSelected?: boolean;
+  isDisabled?: boolean;
+  setSelectedTab: (index: number) => void;
+}
+
+export interface TabPanelProps {
+  children: React.ReactNode;
+  isDisabled?: boolean;
+  title: string;
 }
--- src/stories/tabs/index.stories.tsx
+++ src/stories/tabs/index.stories.tsx
@@ -1,55 +1,27 @@
 import { ComponentMeta, Story } from "@storybook/react";
-import { TabPanel } from "@zendeskgarden/react-tabs";
-import { Tabs } from ".";
+import { Tabs } from "./index";
 import { Col } from "../grid/col";
 import { Row } from "../grid/row";
-import { CustomTabs } from "./customTabs";
-import { CustomTabsArgs, TabsArgs } from "./_types";
+import { TabsArgs } from "./_types";
+
 
 const Template: Story<TabsArgs> = (args) => (
   <Row>
     <Col>
       <Tabs {...args}>
-        <Tabs.List>
-          <Tabs.Tab item="tab-1">Tab 1</Tabs.Tab>
-          <Tabs.Tab item="tab-2">Tab 2</Tabs.Tab>
-          <Tabs.Tab item="tab-3">Tab 3</Tabs.Tab>
-        </Tabs.List>
-        <Tabs.Panel item="tab-1">Tab 1 content</Tabs.Panel>
-        <Tabs.Panel item="tab-2">Tab 2 content</Tabs.Panel>
-        <Tabs.Panel item="tab-3">Tab 3 content</Tabs.Panel>
+        <Tabs.Panel title="Tab 1">Tab 1 content</Tabs.Panel>
+        <Tabs.Panel title="Tab 2">Tab 2 content</Tabs.Panel>
+        <Tabs.Panel isDisabled title="Tab 3">Tab 3 content</Tabs.Panel>
       </Tabs>
     </Col>
   </Row>
 );
 
-const TemplateBuyedOnWish: Story<CustomTabsArgs> = (args) => (
-  <Row>
-    <Col>
-      <CustomTabs {...args}>
-        <TabPanel title="Tab 1">Tab 1 content</TabPanel>
-        <TabPanel title="Tab 2">Tab 2 content</TabPanel>
-        <TabPanel title="Tab 3">Tab 3 content</TabPanel>
-      </CustomTabs>
-    </Col>
-  </Row>
-);
 
-export const Horizontal = Template.bind({});
-Horizontal.args = {
-  onChange: (item) => console.log(item),
-};
-
-export const Vertical = Template.bind({});
-Vertical.args = {
-  isVertical: true,
-  selectedItem: "tab-1",
-  onChange: (item) => console.log(item),
-};
-
-export const BuyedOnWish = TemplateBuyedOnWish.bind({});
-BuyedOnWish.args = {
-  selectedIndex: 2,
+export const Default = Template.bind({});
+Default.args = {
+  selectedIndex: 1,
+  onTabChange: (index) => console.log("⭐ Tab:", index),
 };
 
 export default {
