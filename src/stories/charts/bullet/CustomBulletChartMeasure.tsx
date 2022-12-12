237c7163d741890aeb1d94796a2639a5b471a4c5
Iacopo Leardini <iacopo.lea@gmail.com>
2022-12-12 11:24:06 +0100

    fix(bullet): UN-209 animate measue component

--- src/stories/charts/bullet/CustomBulletChartMeasure.tsx
+++ src/stories/charts/bullet/CustomBulletChartMeasure.tsx
@@ -1,4 +1,5 @@
 import { CHARTS_COLOR_PALETTE } from "../../theme/charts";
+import { animated } from "@react-spring/web";
 
 export interface CustomBulletChartMeasureProps {
   x: number;
@@ -9,7 +10,7 @@ export interface CustomBulletChartMeasureProps {
 };
 
 export const CustomMeasure = ({ x, y, width, height }: CustomBulletChartMeasureProps) => (
-  <rect
+  <animated.rect
     x={x + 2}
     y={y + 2}
     width={width - 4}
