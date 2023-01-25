8a971376437317f5a43b1a830a22ccb3af83fd7e
Marco Bonomo <marco.bonomo@unguess.io>
2023-01-25 18:28:15 +0100

    feat(player): add support for ios fullscreen

--- src/stories/player/parts/fullScreenButton.tsx
+++ src/stories/player/parts/fullScreenButton.tsx
@@ -2,13 +2,38 @@ import { IconButton } from "../../buttons/icon-button";
 import { ReactComponent as FullScreenIcon } from "../../../assets/icons/maximize-fill.svg";
 import { useCallback } from "react";
 
+interface VideowithFullscreen extends HTMLVideoElement {
+  webkitEnterFullscreen?: () => Promise<void>;
+  webkitRequestFullscreen?: () => Promise<void>;
+  mozRequestFullScreen?: () => Promise<void>;
+  msRequestFullscreen?: () => Promise<void>;
+}
+
 export const FullScreenButton = (props: {
-  videoRef: HTMLVideoElement | null;
+  videoRef: VideowithFullscreen | null;
 }) => {
+  const { videoRef } = props;
+  const {
+    requestFullscreen,
+    webkitRequestFullscreen,
+    mozRequestFullScreen,
+    webkitEnterFullscreen,
+    msRequestFullscreen,
+  } = videoRef || {};
+
   const handleFullScreen = useCallback(async () => {
     if (props.videoRef) {
       if (props.videoRef.requestFullscreen) {
         await props.videoRef.requestFullscreen();
+      } else if (props.videoRef.webkitRequestFullscreen) {
+        await props.videoRef.webkitRequestFullscreen();
+      } else if (props.videoRef.mozRequestFullScreen) {
+        await props.videoRef.mozRequestFullScreen();
+      } else if (props.videoRef.webkitEnterFullscreen) {
+        // iOS
+        await props.videoRef.webkitEnterFullscreen();
+      } else if (props.videoRef.msRequestFullscreen) {
+        await props.videoRef.msRequestFullscreen();
       }
     }
   }, [props.videoRef]);
@@ -20,6 +45,13 @@ export const FullScreenButton = (props: {
         handleFullScreen();
         e.stopPropagation();
       }}
+      disabled={
+        !requestFullscreen &&
+        !webkitRequestFullscreen &&
+        !mozRequestFullScreen &&
+        !webkitEnterFullscreen &&
+        !msRequestFullscreen
+      }
     >
       <FullScreenIcon />
     </IconButton>
