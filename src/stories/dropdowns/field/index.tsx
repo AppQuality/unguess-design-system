2d22da0473aaa6920d9075a56fc230911dc37cf4
Marco Bonomo <marco.bonomo@unguess.io>
2023-01-30 13:14:32 +0100

    fix(dropdowns-field): fix dropdowns field component width auto

--- src/stories/dropdowns/field/index.tsx
+++ src/stories/dropdowns/field/index.tsx
@@ -1,6 +1,8 @@
 import { Field as ZendeskField } from "@zendeskgarden/react-dropdowns";
 import styled from "styled-components";
 
-const Field = styled(ZendeskField)``;
+const Field = styled(ZendeskField)`
+    display: inline-block;
+`;
 
 export { Field };
