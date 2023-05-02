159877186732b81aad11dc7d3fcd815f4d320999
Marco Bonomo <marco.bonomo@unguess.io>
2023-05-02 10:34:33 +0200

    🔨 refactor(notifications): change Notification component to use styled-components 🔨 refactor(notifications): remove isRegular prop from ToastProviderArgs 🎨 style(notifications): add styles to Notification component ✨ feat(notifications): add closeText prop to NotificationArgs ✨ feat(notifications): add onClose prop to NotificationArgs ✨ feat(notifications): add children prop to NotificationArgs ✨ feat(notifications): add Toasts component to show notifications with useToast hook

--- src/stories/notifications/index.tsx
+++ src/stories/notifications/index.tsx
@@ -4,6 +4,72 @@ import {
   useToast as ZendeskUseToast,
 } from "@zendeskgarden/react-notifications";
 import { NotificationArgs, ToastProviderArgs } from "./_types";
+import styled from "styled-components";
+import { Anchor } from "@zendeskgarden/react-buttons";
+
+const UgAnchor = styled(Anchor)`
+  cursor: pointer;
+`;
+
+const UgNotification = styled(ZendeskNotification)<NotificationArgs>`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  > svg {
+    margin-top: 1px;
+  }
+
+  ${UgAnchor} {
+    flex-shrink: 0;
+    margin-left: ${({ theme }) => theme.space.md};
+  }
+
+  ${(props) =>
+    props.type === ("success" as NotificationArgs["type"]) &&
+    `
+    background-color: ${props.theme.palette.green[700]};
+    color: ${props.theme.palette.white};
+
+    *, *:hover, *:focus, *:active {
+      color: ${props.theme.palette.white} !important;
+    }
+  `}
+
+  ${(props) =>
+    props.type === ("error" as NotificationArgs["type"]) &&
+    `
+    background-color: ${props.theme.palette.red[600]};
+    color: ${props.theme.palette.white};
+
+    *, *:hover, *:focus, *:active {
+      color: ${props.theme.palette.white} !important;
+    }
+  `}
+
+  ${(props) =>
+    props.type === ("warning" as NotificationArgs["type"]) &&
+    `
+    background-color: ${props.theme.palette.yellow[600]};
+    color: ${props.theme.palette.white};
+
+    *, *:hover, *:focus, *:active {
+      color: ${props.theme.palette.white} !important;
+    }
+  `}
+
+  ${(props) =>
+    props.type === ("info" as NotificationArgs["type"]) &&
+    props.isPrimary &&
+    `
+    background-color: ${props.theme.palette.blue[600]};
+    color: ${props.theme.palette.white};
+
+    *, *:hover, *:focus, *:active {
+      color: ${props.theme.palette.white} !important;
+    }
+  `}
+`;
 
 /**
  * A Notification is a passive status update that keeps users informed of system progress.
@@ -11,12 +77,22 @@ import { NotificationArgs, ToastProviderArgs } from "./_types";
  * Used for this:
     - For a passive status update about user or system activity
  */
-const Notification = (props: NotificationArgs) => (
-  <ZendeskNotification {...props} />
+const Notification = ({
+  children,
+  closeText,
+  onClose,
+  ...props
+}: NotificationArgs) => (
+  <UgNotification {...props}>
+    {children}
+    <UgAnchor onClick={onClose}>{closeText ?? "Dismiss"}</UgAnchor>
+  </UgNotification>
 );
 
-//Extras
-const ToastProvider = (props: ToastProviderArgs) => <ZendeskToastProvider {...props}/>;
+// ToastProvider
+const ToastProvider = (props: ToastProviderArgs) => (
+  <ZendeskToastProvider {...props} />
+);
 const useToast = ZendeskUseToast;
 
 export { Notification, ToastProvider, useToast };
