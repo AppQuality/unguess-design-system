54c54ddbb56b01299aba7805156e0a19983988f9
Iacopo Leardini <iacopo.lea@gmail.com>
2023-05-16 12:55:35 +0200

    feat(colors): typography

--- src/stories/typography/paragraph/index.tsx
+++ src/stories/typography/paragraph/index.tsx
@@ -3,12 +3,13 @@ import {
 } from "@zendeskgarden/react-typography";
 import styled from "styled-components";
 import { ParagraphArgs } from "./_types";
+import { palette } from "../../theme/palette";
 
 const UgParagraph = styled(ZendeskParagraph)``;
 
 /**
  *  Use Paragraph to render blocks of text with Garden styling.
  */
-const Paragraph = (props: ParagraphArgs) => <UgParagraph {...props} />;
+const Paragraph = (props: ParagraphArgs) => <UgParagraph {...props} style={{color: palette.grey[700]}} />;
 
 export { Paragraph};
--- src/stories/typography/typescale/index.tsx
+++ src/stories/typography/typescale/index.tsx
@@ -4,20 +4,45 @@ import {
   LG as ZendeskLG,
   XL as ZendeskXL,
   XXL as ZendeskXXL,
-  XXXL as ZendeskXXXL,
+  XXXL as ZendeskXXXL
 } from "@zendeskgarden/react-typography";
+import styled from "styled-components";
+import { ITypescaleMonospaceProps } from "@zendeskgarden/react-typography/dist/typings/types";
 import { theme } from "../../theme";
-import { SMArgs, MDArgs, LGArgs, XLArgs, XXLArgs, XXXLArgs } from "./_types";
 
 /**
  *  Type components come in small, medium, large, extra-large, extra-extra-large, and extra-extra-extra-large.
  */
-const SM = (props: SMArgs) => <ZendeskSM style={props.isMonospace ? { lineHeight: theme.lineHeights.sm } : {}} {...props}/>;
-const MD = (props: MDArgs) => <ZendeskMD style={props.isMonospace ? { lineHeight: theme.lineHeights.md } : {}} {...props}/>;
-const LG = (props: LGArgs) => <ZendeskLG style={props.isMonospace ? { lineHeight: theme.lineHeights.lg } : {}} {...props}/>;
-const XL = (props: XLArgs) => <ZendeskXL {...props}/>;
-const XXL = (props: XXLArgs) => <ZendeskXXL {...props}/>;
-const XXXL = (props: XXXLArgs) => <ZendeskXXXL {...props}/>;
+const SM = styled(ZendeskSM)`
+  color: ${p => p.theme.palette.grey[800]};
+`;
+const MD = styled(ZendeskMD)`
+  color: ${p => p.theme.palette.grey[800]};
+`;
+const LG = styled(ZendeskLG)`
+  color: ${p => p.theme.palette.grey[800]};
+`;
+const XL = styled(ZendeskXL)`
+  color: ${p => p.theme.palette.grey[800]};
+`;
+const XXL = styled(ZendeskXXL)`
+  color: ${p => p.theme.palette.grey[800]};
+`;
+const XXXL = styled(ZendeskXXXL)`
+  color: ${p => p.theme.palette.blue[600]};
+`;
+const TextLabel = styled(ZendeskSM)`
+  color: ${ p => p.theme.palette.grey[600]};
+`;
+interface TextDescriptionProps extends ITypescaleMonospaceProps {
+  isSmall?: boolean;
+}
+const TextDescription = ({isSmall, ...props}: TextDescriptionProps) => {
+  return (
+    {isSmall}
+     ? <SM style={{color: theme.palette.grey[700]}} {...props} />
+     : <MD style={{color: theme.palette.grey[700]}} {...props} />
+  )
+}
 
-
-export { SM, MD, LG, XL, XXL, XXXL };
+export { SM, MD, LG, XL, XXL, XXXL, TextLabel, TextDescription };
