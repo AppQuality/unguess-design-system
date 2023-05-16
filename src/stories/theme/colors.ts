977624e6be4a17a2f0fd47b8c4f718c2a62698fd
Iacopo Leardini <iacopo.lea@gmail.com>
2023-05-16 16:38:59 +0200

    feat(colors): theme colors type

--- src/stories/theme/colors.ts
+++ src/stories/theme/colors.ts
@@ -1,8 +1,12 @@
 import { DEFAULT_THEME } from "@zendeskgarden/react-theming";
+type ThemeColors = typeof DEFAULT_THEME.colors & {
+  infoHue: string;
+  accentHue: string;
+};
 
-export const colors = {
+export const colors: ThemeColors = {
   ...DEFAULT_THEME.colors, // we are overwriting this but want to keep the original colors as reference
-  base: "light",
+  base: 'light',
   background: "#fff",
   foreground: "#2f3941",
   primaryHue: "blue",
