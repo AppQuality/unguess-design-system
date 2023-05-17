219554c747310a85c540bece7f05811ee2f15330
Iacopo Leardini <iacopo.lea@gmail.com>
2023-05-17 18:15:42 +0200

    fix(colors): typescale

--- src/stories/typography/typescale/index.tsx
+++ src/stories/typography/typescale/index.tsx
@@ -13,26 +13,26 @@ import { theme } from "../../theme";
 /**
  *  Type components come in small, medium, large, extra-large, extra-extra-large, and extra-extra-extra-large.
  */
-const SM = styled(ZendeskSM)`
-  color: ${p => p.theme.palette.grey[800]};
+const SM = styled(ZendeskSM)<{color?: string}>`
+  color: ${p => p.color || p.theme.palette.grey[800]};
 `;
-const MD = styled(ZendeskMD)`
-  color: ${p => p.theme.palette.grey[800]};
+const MD = styled(ZendeskMD)<{color?: string}>`
+  color: ${p => p.color || p.theme.palette.grey[800]};
 `;
-const LG = styled(ZendeskLG)`
-  color: ${p => p.theme.palette.grey[800]};
+const LG = styled(ZendeskLG)<{color?: string}>`
+  color: ${p => p.color || p.theme.palette.grey[800]};
 `;
-const XL = styled(ZendeskXL)`
-  color: ${p => p.theme.palette.grey[800]};
+const XL = styled(ZendeskXL)<{color?: string}>`
+  color: ${p => p.color || p.theme.palette.grey[800]};
 `;
-const XXL = styled(ZendeskXXL)`
-  color: ${p => p.theme.palette.grey[800]};
+const XXL = styled(ZendeskXXL)<{color?: string}>`
+  color: ${p => p.color || p.theme.palette.grey[800]};
 `;
-const XXXL = styled(ZendeskXXXL)`
-  color: ${p => p.theme.palette.blue[600]};
+const XXXL = styled(ZendeskXXXL)<{color?: string}>`
+  color: ${p => p.color || p.theme.palette.blue[600]};
 `;
-const TextLabel = styled(ZendeskSM)`
-  color: ${ p => p.theme.palette.grey[600]};
+const TextLabel = styled(ZendeskSM)<{color?: string}>`
+  color: ${ p => p.color || p.theme.palette.grey[600]};
 `;
 interface TextDescriptionProps extends ITypescaleMonospaceProps {
   isSmall?: boolean;
@@ -40,8 +40,8 @@ interface TextDescriptionProps extends ITypescaleMonospaceProps {
 const TextDescription = ({isSmall, ...props}: TextDescriptionProps) => {
   return (
     {isSmall}
-     ? <SM style={{color: theme.palette.grey[700]}} {...props} />
-     : <MD style={{color: theme.palette.grey[700]}} {...props} />
+     ? <SM color={theme.palette.grey[700]} {...props} />
+     : <MD color={theme.palette.grey[700]} {...props} />
   )
 }
 
