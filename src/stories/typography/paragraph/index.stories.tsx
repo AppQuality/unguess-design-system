c755a37c067e1f20d321cbf5db925c8d27a810c6
Iacopo Leardini <iacopo.lea@gmail.com>
2023-05-18 10:46:57 +0200

    feat(colors): paragraph color

--- src/stories/typography/paragraph/index.stories.tsx
+++ src/stories/typography/paragraph/index.stories.tsx
@@ -1,9 +1,9 @@
 import { ComponentMeta, Story } from "@storybook/react";
 import { Paragraph } from ".";
 import { MD } from "../typescale";
-import { ParagraphArgs } from "./_types";
+import { IParagraphProps } from "@zendeskgarden/react-typography";
 
-const Template: Story<ParagraphArgs> = (args) => {
+const Template: Story<IParagraphProps> = (args) => {
   return (
     <>
       <Paragraph {...args}>
@@ -16,7 +16,7 @@ const Template: Story<ParagraphArgs> = (args) => {
   );
 };
 
-const defaultArgs: ParagraphArgs = {
+const defaultArgs: IParagraphProps = {
   children:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 };
--- src/stories/typography/paragraph/index.tsx
+++ src/stories/typography/paragraph/index.tsx
@@ -1,15 +1,18 @@
 import {
+  IParagraphProps,
   Paragraph as ZendeskParagraph
 } from "@zendeskgarden/react-typography";
 import styled from "styled-components";
-import { ParagraphArgs } from "./_types";
-import { palette } from "../../theme/palette";
 
-const UgParagraph = styled(ZendeskParagraph)``;
+const UgParagraph = styled(ZendeskParagraph)`
+  > div, p {
+    color: ${p => p.color || p.theme.palette.grey[700]};
+  }
+`;
 
 /**
  *  Use Paragraph to render blocks of text with Garden styling.
  */
-const Paragraph = (props: ParagraphArgs) => <UgParagraph {...props} style={{color: palette.grey[700]}} />;
+const Paragraph = (props: IParagraphProps) => <UgParagraph {...props} />;
 
 export { Paragraph};
