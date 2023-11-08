a964bbadaafc4a5f6a0667c95c05561a33e4f538
Marco Bonomo <marco.bonomo@unguess.io>
2023-11-08 17:19:19 +0100

    🔀 refactor(color-swatch/_types.ts): update ColorSwatchProps interface and add ColorSwatchTriggerProps interface

--- src/stories/color-swatch/_types.ts
+++ src/stories/color-swatch/_types.ts
@@ -1,9 +1,15 @@
-export interface ColorSwatchProps {
-    colors: {label:string, value:string}[];
-    initialRowIndex:number;
-    initialColIndex:number;
+import { IColorSwatchDialogProps } from "@zendeskgarden/react-colorpickers";
+
+export interface ColorSwatchProps extends Omit<IColorSwatchDialogProps, "colors"> {
+    colors: { 
+        label: string; 
+        value: string;
+    }[];
+    children?: React.ReactNode | React.ReactNode[];
 }
 
-export interface ColorSwatchIconButtonProps {
-    iconColor: string;
-  }
+export interface ColorSwatchTriggerProps {
+    color: string;
+    style?: React.CSSProperties;
+    children?: React.ReactNode | React.ReactNode[];
+}
