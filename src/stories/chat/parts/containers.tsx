fa0857b2445d73e8b33decce2126b76f20f75482
Luca Cannarozzo <cannarozzoluca@live.it>
2023-12-28 00:14:01 +0100

    fix(chat): remove function from styled component

--- src/stories/chat/parts/containers.tsx
+++ src/stories/chat/parts/containers.tsx
@@ -11,24 +11,11 @@ export const ChatContainer = styled(Card)`
   cursor: default;
 `;
 
-const parseBackground = (props: ChatArgs) => {
-  if (!props.chatBkg) return `url(${defaultBkg}) repeat center center`;
-
-  if (
-    props.chatBkg.startsWith("#") ||
-    props.chatBkg.startsWith("rgb") ||
-    props.chatBkg.startsWith("hsl")
-  ) {
-    return props.chatBkg;
-  }
-  return `url(${props.chatBkg}) repeat center center`;
-};
-
 export const MessagesContainer = styled.div<ChatArgs>`
   padding: ${({ theme }) => theme.space.md};
   margin: ${({ theme }) => `0 -${theme.space.md}`};
   display: flex;
   flex-direction: column;
   gap: ${({ theme }) => theme.space.sm};
-  background: ${parseBackground};
+  background: ${({ chatBkg }) => chatBkg ?? `url(${defaultBkg}) repeat center center`};
 `;
