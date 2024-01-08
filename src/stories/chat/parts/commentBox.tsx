e75038f15d6e0f043e69054521510df131ccf822
Luca Cannarozzo <cannarozzoluca@live.it>
2024-01-08 17:34:07 +0100

    wip(chat-mentions): add types and components for mentions functionality

--- src/stories/chat/parts/commentBox.tsx
+++ src/stories/chat/parts/commentBox.tsx
@@ -3,6 +3,7 @@ import {
   EditorContent,
   Editor as TipTapEditor,
   Content,
+  ReactRenderer,
 } from "@tiptap/react";
 import styled from "styled-components";
 
@@ -11,8 +12,8 @@ import Link from "@tiptap/extension-link";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
 import CharacterCount from "@tiptap/extension-character-count";
-import Highlight from '@tiptap/extension-highlight'
-import TextAlign from '@tiptap/extension-text-align'
+import Highlight from "@tiptap/extension-highlight";
+import TextAlign from "@tiptap/extension-text-align";
 
 import { editorStyle } from "../../shared/editorStyle";
 import { ChatArgs, ChatEditorArgs } from "../_types";
@@ -22,6 +23,9 @@ import { FauxInput } from "@zendeskgarden/react-forms";
 import { Avatar } from "../../avatar";
 import { useChatContext } from "../context/chatContext";
 import { CommentBar } from "./bar";
+import Mention from "@tiptap/extension-mention";
+import { MentionList, MentionListRef, SuggestedUser } from "./mentionList";
+import tippy, { type Instance as TippyInstance } from "tippy.js";
 
 const ChatBoxContainer = styled.div`
   display: flex;
@@ -43,6 +47,29 @@ const EditorContainer = styled(FauxInput)<ChatArgs>`
     ${editorStyle}
   }
 `;
+
+/**
+ * Workaround for the current typing incompatibility between Tippy.js and Tiptap
+ * Suggestion utility.
+ *
+ * @see https://github.com/ueberdosis/tiptap/issues/2795#issuecomment-1160623792
+ *
+ * Adopted from
+ * https://github.com/Doist/typist/blob/a1726a6be089e3e1452def641dfcfc622ac3e942/stories/typist-editor/constants/suggestions.ts#L169-L186
+ */
+const DOM_RECT_FALLBACK: DOMRect = {
+  bottom: 0,
+  height: 0,
+  left: 0,
+  right: 0,
+  top: 0,
+  width: 0,
+  x: 0,
+  y: 0,
+  toJSON() {
+    return {};
+  },
+};
 
 /**
  * CommentBox is a wrapper around Editor component 
@@ -90,6 +117,98 @@ export const CommentBox = ({
       Highlight,
       TextAlign.configure({
         types: ["heading", "paragraph"],
+      }),
+      Mention.configure({
+        HTMLAttributes: {
+          class: "mention",
+        },
+        suggestion: {
+          items: ({ query }): SuggestedUser[] => {
+            console.log("🚀 ~ file: commentBox.tsx:158 ~ query:", query);
+            return [
+              {
+                id: 1,
+                name: "John Doe",
+                avatar: "https://i.pravatar.cc/150?img=1",
+              },
+              {
+                id: 2,
+                name: "Jane Doe",
+                avatar: "https://i.pravatar.cc/150?img=2",
+              },
+            ].filter((item) => {
+              if (!query) return item;
+              return item.name.toLowerCase().startsWith(query.toLowerCase());
+            });
+          },
+          render: () => {
+            let component: ReactRenderer<MentionListRef> | undefined;
+            let popup: TippyInstance | undefined;
+
+            return {
+              onStart: (props) => {
+                component = new ReactRenderer(MentionList, {
+                  props,
+                  editor: props.editor,
+                });
+
+                if (!props.clientRect) {
+                  return;
+                }
+
+                popup = tippy("body", {
+                  getReferenceClientRect: () =>
+                    props.clientRect?.() ?? DOM_RECT_FALLBACK,
+                  appendTo: () => document.body,
+                  content: component.element,
+                  showOnCreate: true,
+                  interactive: true,
+                  trigger: "manual",
+                  placement: "bottom-start",
+                })[0];
+              },
+
+              onUpdate(props) {
+                component?.updateProps(props);
+
+                if (!props.clientRect) {
+                  return;
+                }
+
+                popup?.setProps({
+                  getReferenceClientRect: () =>
+                    props.clientRect?.() ?? DOM_RECT_FALLBACK,
+                });
+              },
+
+              onKeyDown(props) {
+                if (props.event.key === "Escape") {
+                  popup?.hide();
+
+                  return true;
+                }
+
+                if (!component?.ref) {
+                  return false;
+                }
+                return component.ref.onKeyDown(props);
+              },
+
+              onExit() {
+                popup?.destroy();
+                component?.destroy();
+
+                // Remove references to the old popup and component upon destruction/exit.
+                // (This should prevent redundant calls to `popup.destroy()`, which Tippy
+                // warns in the console is a sign of a memory leak, as the `suggestion`
+                // plugin seems to call `onExit` both when a suggestion menu is closed after
+                // a user chooses an option, *and* when the editor itself is destroyed.)
+                popup = undefined;
+                component = undefined;
+              },
+            };
+          },
+        },
       }),
     ],
     content: (children as Content) || "",
---
+++ src/stories/chat/parts/mentionList.tsx
@@ -0,0 +1,95 @@
+import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
+import type { SuggestionOptions, SuggestionProps } from "@tiptap/suggestion";
+
+export type MentionListRef = {
+  onKeyDown: NonNullable<
+    ReturnType<
+      NonNullable<SuggestionOptions<SuggestedUser>["render"]>
+    >["onKeyDown"]
+  >;
+};
+
+export type SuggestedUser = { id: number; name: string; avatar: string };
+
+type MentionListProps = SuggestionProps<SuggestedUser>;
+
+export const MentionList = forwardRef<MentionListRef, MentionListProps>(
+  (props, ref) => {
+    const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
+    const selectItem = (index: number) => {
+      const item = props.items[index];
+
+      if (item) {
+        props.command(item);
+      }
+    };
+
+    const upHandler = () => {
+      console.log(
+        "🚀 ~ file: mentionList.tsx:126 ~ upHandler ~ selectedIndex",
+        selectedIndex
+      );
+      setSelectedIndex(
+        (selectedIndex + props.items.length - 1) % props.items.length
+      );
+    };
+
+    const downHandler = () => {
+      console.log(
+        "🚀 ~ file: mentionList.tsx:133 ~ downHandler ~ selectedIndex",
+        selectedIndex
+      );
+      setSelectedIndex((selectedIndex + 1) % props.items.length);
+    };
+
+    const enterHandler = () => {
+      console.log(
+        "🚀 ~ file: mentionList.tsx:140 ~ enterHandler ~ selectedIndex",
+        selectedIndex
+      );
+      selectItem(selectedIndex);
+    };
+
+    useEffect(() => setSelectedIndex(0), [props.items]);
+
+    useImperativeHandle(ref, () => ({
+      onKeyDown: ({ event }) => {
+        if (event.key === "ArrowUp") {
+          upHandler();
+          return true;
+        }
+
+        if (event.key === "ArrowDown") {
+          downHandler();
+          return true;
+        }
+
+        if (event.key === "Enter") {
+          enterHandler();
+          return true;
+        }
+
+        return false;
+      },
+    }));
+
+    return (
+      <div className="items">
+        {props.items.length ? (
+          props.items.map((item, index) => (
+            <button
+              className={`item ${index === selectedIndex ? "is-selected" : ""}`}
+              key={index}
+              onClick={() => selectItem(index)}
+            >
+              {item.name}
+            </button>
+          ))
+        ) : (
+          <div className="item">No result</div>
+        )}
+      </div>
+    );
+  }
+);
