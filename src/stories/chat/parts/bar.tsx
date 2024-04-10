13f9a88f0ad56c26e74a81f09c765d9e5d296a1a
Kariamos <minucci9292@gmail.com>
2024-04-10 15:53:29 +0200

    wip

--- src/stories/chat/parts/bar.tsx
+++ src/stories/chat/parts/bar.tsx
@@ -6,8 +6,9 @@ import { isMac } from "../../theme/utils";
 import { ReactComponent as BoldIcon } from "../../../assets/icons/bold-fill.svg";
 import { ReactComponent as ItalicIcon } from "../../../assets/icons/italic-fill.svg";
 import { ReactComponent as MentionIcon } from "../../../assets/icons/at-fill.svg";
-import { ReactComponent as AttachmentIcon } from "../../../assets/icons/file-image-stroke.svg";
+import { ReactComponent as AttachmentIcon } from "../../../assets/icons/clipboard.svg";
 import { IconButton } from "../../buttons/icon-button";
+import { createSingleThumbnail } from "./commentBox";
 
 const MenuContainer = styled.div`
   padding: ${({ theme }) => theme.space.xs} 0;
@@ -69,15 +70,82 @@ const CommentBar = ({
         fileInput.accept = "image/*";
         fileInput.multiple = true;
         fileInput.click();
+
         fileInput.onchange = () => {
           const files = fileInput.files;
           if (files) {
+            const thumbnail_container: HTMLElement | null =
+              document.createElement("div");
+
+            thumbnail_container.className = "thumbnail-container";
+
+            thumbnail_container.style.display = "grid";
+            thumbnail_container.style.gridTemplateColumns = "auto auto auto";
+            thumbnail_container.style.overflowY = "scroll";
+            thumbnail_container.style.justifyItems = "center";
+            thumbnail_container.style.gap = "10px";
+            thumbnail_container.style.backgroundColor = "white";
+            thumbnail_container.style.width = "100%";
+
             Array.from(files).forEach((file) => {
-              editor
-                .chain()
-                .focus()
-                .setImage({ src: URL.createObjectURL(file), alt: file.name })
-                .run();
+              try {
+                // create the thumbnail
+                const img = document.createElement("img");
+                img.src = URL.createObjectURL(file);
+                img.style.width = "100%";
+                img.style.height = "100%";
+                img.style.borderRadius = "10%";
+                const single_thumbnail = createSingleThumbnail(img);
+                //add a red X to the thumbnail to eliminate the thumbnail
+                const x = document.createElement("div");
+                x.style.position = "absolute";
+                x.style.top = "0";
+                x.style.right = "0";
+                x.style.color = "white";
+                x.style.cursor = "pointer";
+                x.style.fontSize = "15px";
+                x.style.fontWeight = "100";
+                x.style.opacity = "0";
+                x.innerHTML = "╳";
+
+                x.style.backgroundColor = "gray";
+                x.style.borderRadius = "50%";
+                x.style.width = "35px";
+                x.style.height = "35px";
+                x.style.display = "flex";
+                x.style.justifyContent = "center";
+                x.style.alignItems = "center";
+
+                single_thumbnail.addEventListener("mouseover", () => {
+                  x.style.opacity = "1";
+                });
+                single_thumbnail.addEventListener("mouseleave", () => {
+                  x.style.opacity = "0";
+                });
+
+                x.addEventListener("click", () => {
+                  thumbnail_container?.removeChild(single_thumbnail);
+                  console.log("delete " + img.src);
+                });
+                single_thumbnail.appendChild(x);
+
+                //add a label with the name of the image
+                const label = document.createElement("div");
+                label.style.position = "absolute";
+                label.style.bottom = "0";
+                label.style.left = "0";
+                label.style.color = "black";
+                label.style.marginBottom = "0";
+                label.style.marginLeft = "5px";
+                label.style.fontSize = "10px";
+                label.style.fontWeight = "400";
+                label.innerHTML = file.name;
+                single_thumbnail.appendChild(label);
+                thumbnail_container?.appendChild(single_thumbnail);
+              } catch (error) {
+                console.error("Error while uploading the image: ", error);
+              }
+              editor.view.dom.parentElement?.appendChild(thumbnail_container);
             });
           }
         };
@@ -146,7 +214,7 @@ const CommentBar = ({
         </IconButton>
       </Tooltip>
       <Tooltip
-        content={i18n?.menu?.attachment ?? "Upload a file"}
+        content={i18n?.menu?.attachment ?? "Upload a file (max size: ...)"}
         placement="top"
         type="light"
         size="small"
--- src/stories/chat/parts/commentBox.tsx
+++ src/stories/chat/parts/commentBox.tsx
@@ -6,6 +6,7 @@ import {
   Content,
 } from "@tiptap/react";
 import { ChatEditorArgs } from "../_types";
+import { createPortal } from "react-dom";
 import { KeyboardEvent as ReactKeyboardEvent, PropsWithChildren } from "react";
 
 import { FloatingMenu } from "../../editor/floatingMenu";
@@ -13,6 +14,10 @@ import { useChatContext } from "../context/chatContext";
 import { CommentBar } from "./bar";
 import { editorExtensions } from "./extensions";
 import { EditorContainer } from "./containers";
+import { EditorView } from "@tiptap/pm/view";
+import { LightboxArgs } from "../../lightbox/_types";
+import { Lightbox } from "../../lightbox";
+import ThumbnailContainer from "./ThumbnailContainer";
 
 const ChatBoxContainer = styled.div`
   display: flex;
@@ -57,6 +62,9 @@ export const CommentBox = ({
       handleDrop: function (view, event, slice, moved) {
         if (!event.dataTransfer || !event.dataTransfer.files) return false;
 
+        const thumbnail_container: HTMLElement | null =
+          createThumbnailContainer();
+
         const files = Array.from(event.dataTransfer.files);
         const imageFiles = files.filter((file) => /^image\//.test(file.type));
 
@@ -64,20 +72,32 @@ export const CommentBox = ({
 
         event.preventDefault();
 
+        view.dom.parentElement?.appendChild(thumbnail_container);
+
+        /*if (view.dom.parentElement) {
+          window.alert("create portal");
+          createPortal(<ThumbnailContainer />, view.dom.parentElement);
+        }*/
         imageFiles.forEach(async (file) => {
           try {
-            const imageDataUrl = URL.createObjectURL(file);
-            const node = view.state.schema.nodes.image.create({
-              src: imageDataUrl,
-            });
+            // create the thumbnail
 
-            const transaction = view.state.tr.replaceSelectionWith(node);
-            view.dispatch(transaction);
-          } catch (error) {
-            console.error(
-              "Errore durante il caricamento dell'immagine:",
-              error
+            const img = createThumbnailImage(file);
+            const single_thumbnail = createSingleThumbnail(img);
+            const x = deleteThumbnailAction(
+              single_thumbnail,
+              thumbnail_container,
+              img
             );
+
+            console.log("carico su s3");
+
+            const label = createMediaLabel(file);
+            single_thumbnail.appendChild(x);
+            single_thumbnail.appendChild(label);
+            thumbnail_container?.appendChild(single_thumbnail);
+          } catch (error) {
+            console.error("Error while uploading the image: ", error);
           }
         });
 
@@ -127,6 +147,7 @@ export const CommentBox = ({
     if ((event.ctrlKey || event.metaKey) && event.key === "Enter") {
       triggerSave();
       editor?.commands.clearContent();
+      editor?.commands.deleteNode("thumbnail-container");
     }
   };
 
@@ -148,4 +169,113 @@ export const CommentBox = ({
       {hasButtonsMenu && <CommentBar editor={ed} i18n={i18n} />}
     </>
   );
+};
+function createMediaLabel(file: File) {
+  const label = document.createElement("div");
+  label.style.position = "absolute";
+  label.style.bottom = "0";
+  label.style.left = "0";
+  label.style.color = "black";
+  label.style.marginBottom = "0";
+  label.style.marginLeft = "5px";
+  label.style.fontSize = "10px";
+  label.style.fontWeight = "400";
+  label.innerHTML = file.name;
+  return label;
+}
+
+function deleteThumbnailAction(
+  single_thumbnail: HTMLDivElement,
+  thumbnail_container: HTMLElement,
+  img: HTMLImageElement
+) {
+  const x = document.createElement("div");
+  x.style.position = "absolute";
+  x.style.top = "0";
+  x.style.right = "0";
+  x.style.color = "white";
+  x.style.cursor = "pointer";
+  x.style.fontSize = "15px";
+  x.style.fontWeight = "100";
+  x.style.opacity = "0";
+  x.innerHTML = "╳";
+
+  x.style.backgroundColor = "gray";
+  x.style.borderRadius = "50%";
+  x.style.width = "35px";
+  x.style.height = "35px";
+  x.style.display = "flex";
+  x.style.justifyContent = "center";
+  x.style.alignItems = "center";
+
+  single_thumbnail.addEventListener("mouseover", () => {
+    x.style.opacity = "1";
+  });
+  single_thumbnail.addEventListener("mouseleave", () => {
+    x.style.opacity = "0";
+  });
+
+  x.addEventListener("click", () => {
+    thumbnail_container?.removeChild(single_thumbnail);
+    console.log("delete " + img.src);
+  });
+  return x;
+}
+
+function createThumbnailImage(file: File) {
+  const img = document.createElement("img");
+  img.src = URL.createObjectURL(file);
+  img.style.width = "100%";
+  img.style.height = "100%";
+  img.style.borderRadius = "10%";
+  return img;
+}
+
+function createThumbnailContainer() {
+  const thumbnail_container = document.createElement("div");
+  thumbnail_container.className = "thumbnail-container";
+
+  thumbnail_container.style.display = "grid";
+  thumbnail_container.style.gridTemplateColumns = "auto auto auto";
+  thumbnail_container.style.overflowY = "scroll";
+  thumbnail_container.style.justifyItems = "center";
+  thumbnail_container.style.gap = "10px";
+  thumbnail_container.style.backgroundColor = "white";
+  thumbnail_container.style.width = "100%";
+  thumbnail_container.style.height = "150px";
+
+  return thumbnail_container;
+}
+
+function createSingleThumbnail(img: HTMLImageElement) {
+  const single_thumbnail = document.createElement("div");
+  single_thumbnail.className = "thumbnail";
+  single_thumbnail.style.position = "relative";
+  single_thumbnail.style.justifyContent = "center";
+  single_thumbnail.style.alignItems = "stretch";
+  single_thumbnail.style.backgroundColor = "rgba(0,0,0,0.1)";
+  single_thumbnail.style.maxHeight = "120px";
+  single_thumbnail.style.maxWidth = "120px";
+  single_thumbnail.style.minHeight = "90px";
+  single_thumbnail.style.minWidth = "90px";
+  single_thumbnail.style.padding = "5px 5px 15px 5px";
+  single_thumbnail.style.borderRadius = "10%";
+  single_thumbnail.appendChild(img);
+  return single_thumbnail;
+}
+
+function showImageInline(file: File, view: EditorView) {
+  const imageDataUrl = URL.createObjectURL(file);
+  const node = view.state.schema.nodes.image.create({
+    src: imageDataUrl,
+  });
+  const transaction = view.state.tr.replaceSelectionWith(node);
+  view.dispatch(transaction);
+}
+
+export {
+  createSingleThumbnail,
+  createThumbnailContainer,
+  createThumbnailImage,
+  deleteThumbnailAction,
 };
