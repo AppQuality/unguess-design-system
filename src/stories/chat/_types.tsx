33db7ed540512b158b72e122f748879b1cfd5488
Kariamos <minucci9292@gmail.com>
2024-05-15 10:30:32 +0200

    refactor: removed unused import

--- src/stories/chat/_types.tsx
+++ src/stories/chat/_types.tsx
@@ -1,6 +1,5 @@
 import { PlaceholderOptions } from "@tiptap/extension-placeholder";
 import { BubbleMenuProps, EditorOptions } from "@tiptap/react";
-import { isError } from "util";
 
 type validationStatus = "success" | "warning" | "error";
 
