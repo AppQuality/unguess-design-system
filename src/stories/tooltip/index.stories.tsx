f1e531f00281ea6eab3c5d7023ad3bc1496351db
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2024-05-15 17:27:01 +0200

    feat: Add isTransparent prop to TooltipArgs interface and implement transparent style in Tooltip component

--- src/stories/tooltip/index.stories.tsx
+++ src/stories/tooltip/index.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentMeta, Story } from "@storybook/react";
 import { Button } from "@zendeskgarden/react-buttons";
 import { Tooltip } from ".";
 import { TooltipArgs } from "./_types";
+import { MD } from "../typography/typescale";
 
 const defaultArgs: TooltipArgs = {
   content: "This is a tooltip",
@@ -17,6 +18,12 @@ const Template: Story<TooltipArgs> = ({ children, ...args }) => {
 export const Default = Template.bind({});
 Default.args = {
   ...defaultArgs,
+};
+
+export const Transparent = Template.bind({});
+Transparent.args = {
+  ...defaultArgs,
+  isTransparent: true,
 };
 
 export default {
