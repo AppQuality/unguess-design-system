17adb324891c1d69259cd9af5da0e099796893fb
Marco Bonomo <marco.bonomo@unguess.io>
2024-06-12 11:57:36 +0200

    ✨ feat(player): add CutStart component to show start point when cutting video

---
+++ src/stories/player/parts/CutStart.tsx
@@ -0,0 +1,30 @@
+import { styled } from "styled-components";
+
+const Pin = styled.div<{ left: number }>`
+  width: 2px;
+  bottom: 0;
+  left: ${({ left }) => `${left}%`};
+  position: absolute;
+  height: 250%;
+  z-index: 2;
+  background-color: ${({ theme }) => theme.palette.kale[600]};
+  margin-left: -2px; // To not override the current time marker
+
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    transform: translate(-50%, 0);
+    background-color: ${({ theme }) => theme.palette.kale[600]};
+  }
+`;
+
+const CutStart = ({ left }: { left: number; }) => {
+  return <Pin id="obs-start-pin" left={left} />;
+};
+
+export { CutStart };
