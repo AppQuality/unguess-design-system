65755703400c7b4582ba1ca1910e200e5dd955af
Davide Bizzi <davide.bizzi@unguess.io>
2024-09-30 16:17:21 +0200

    feat: Use debounced search for highlight

--- src/stories/editorWithHighlight/search.tsx
+++ src/stories/editorWithHighlight/search.tsx
@@ -1,24 +1,42 @@
 import { Editor } from "@tiptap/react";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { css } from "styled-components";
+import { ReactComponent as SearchIcon } from "../../assets/icons/search-stroke.svg";
+import useDebounce from "../../hooks/useDebounce";
+import { MediaInput } from "../forms/mediaInput";
 
-const Search = ({ editor }: { editor: Editor }) => {
+const Search = ({
+  editor,
+  placeholder,
+}: {
+  editor: Editor;
+  placeholder?: string;
+}) => {
   const [search, setSearch] = useState("");
+  const debouncedValue = useDebounce(search, 300);
+
+  const setSearchTerm = useCallback(
+    (term: string) => {
+      editor.commands.setSearchTerm(term);
+    },
+    [editor]
+  );
+
+  useEffect(() => {
+    setSearchTerm(debouncedValue);
+  }, [debouncedValue, setSearchTerm]);
+
   return (
     <>
-      <input
+      <MediaInput
+        isCompact
+        placeholder={placeholder ?? "Search"}
         type="text"
+        start={<SearchIcon />}
         onChange={(e) => {
           setSearch(e.target.value);
         }}
       />
-      <button
-        onClick={() => {
-          editor.commands.setSearchTerm(search);
-        }}
-      >
-        Search
-      </button>
     </>
   );
 };
