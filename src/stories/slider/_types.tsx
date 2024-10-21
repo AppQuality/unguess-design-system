5c949d78453bc62fcae1201e663ecbd4fbc4d86a
Davide Bizzi <davide.bizzi@unguess.io>
2024-10-21 09:23:55 +0200

    chore: Add children prop to SliderArgs interface

--- src/stories/slider/_types.tsx
+++ src/stories/slider/_types.tsx
@@ -3,6 +3,7 @@ import { Settings } from "react-slick";
 export interface SliderArgs extends Settings {
   counter?: boolean;
   onSlideChange?: (currentSlide: number) => void;
+  children?: React.ReactNode;
 }
 
 export interface SliderContextType {
