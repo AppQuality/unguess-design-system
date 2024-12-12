6a5b33ec85f3a43cd391e5939e88c7614f6202d4
Iacopo Leardini <iacopo.lea@gmail.com>
2024-12-12 16:45:37 +0100

    rework: update Alert component to use ZendeskAlert directly with success custom bg

--- src/stories/notifications/alerts/index.tsx
+++ src/stories/notifications/alerts/index.tsx
@@ -1,27 +1,11 @@
 import { Alert as ZendeskAlert, Title as ZendeskTitle, Close as ZendeskClose } from '@zendeskgarden/react-notifications';
-import styled from 'styled-components';
-import { AlertArgs } from './_types';
+import { IAlertProps } from '@zendeskgarden/react-notifications';
 
-const UgAlert = styled(ZendeskAlert)`
-   background-color: white;
-   color: ${({ theme }) => theme.palette.grey[700]};
-   border-width: 2px;
-   font-size: ${({ theme }) => theme.fontSizes.sm};
-`;
 
-const UgAlertTitle = styled(ZendeskTitle)`
-   font-size: ${({ theme }) => theme.fontSizes.md};
-`;
+export interface AlertArgs extends IAlertProps {}
 
-/**
- * Breadcrumbs mark and communicate a user’s location in the product.
- * <hr>
- * Used for this:
-    - To show the user where they are in a nested navigation
-    - To provide a quick way to navigate to ancestor pages
- */
-const Alert = (props: AlertArgs) => <UgAlert {...props}/>;
-Alert.Title = UgAlertTitle;
+const Alert = (props: AlertArgs) => <ZendeskAlert {...props}/>;
+Alert.Title = ZendeskTitle;
 Alert.Close = ZendeskClose;
 
 export { Alert };
