a28cf5a7f641c29ee85f46d573510712898f68ff
Davide Bizzi <davide.bizzi@unguess.io>
2025-03-11 12:43:00 +0100

    refactor: improve value handling in InputToggle component

--- src/stories/forms/input-toggle/_types.tsx
+++ src/stories/forms/input-toggle/_types.tsx
@@ -1,16 +1,17 @@
 import { IInputProps } from "@zendeskgarden/react-forms";
 
-export type textSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl';
+export type textSizes = "xs" | "sm" | "md" | "lg" | "xl" | "xxl" | "xxxl";
 
 export interface InputToggleArgs extends IInputProps {
   /** Removes borders and padding */
   isBare?: boolean;
   /** Applies validation state styling */
-  validation?: 'success' | 'warning' | 'error';
+  validation?: "success" | "warning" | "error";
   /** Sets input size */
   textSize?: textSizes;
   /** Sets the initial focus */
   isFocused?: boolean;
   /** Prevent empty value */
   preventEmpty?: boolean;
+  value?: string;
 }
