c91ae96f8db5c9e0ec7792773917bd93d899fec0
Davide Bizzi <davide.bizzi@unguess.io>
2025-03-28 09:26:20 +0100

    fix: Prevent error on removechild fro bubblemenu when toggling editable state
    https://github.com/ueberdosis/tiptap/issues/3784

--- src/stories/editor/floatingMenu.tsx
+++ src/stories/editor/floatingMenu.tsx
@@ -4,11 +4,11 @@ import { IconButton } from "../buttons/icon-button";
 import { FloatingMenuArgs } from "./_types";
 
 import { ReactComponent as BoldIcon } from "../../assets/icons/bold-fill.svg";
-import { ReactComponent as ItalicIcon } from "../../assets/icons/italic-fill.svg";
-import { ReactComponent as QuoteIcon } from "../../assets/icons/quote-fill.svg";
 import { ReactComponent as H1Icon } from "../../assets/icons/h1-fill.svg";
 import { ReactComponent as H2Icon } from "../../assets/icons/h2-fill.svg";
 import { ReactComponent as H3Icon } from "../../assets/icons/h3-fill.svg";
+import { ReactComponent as ItalicIcon } from "../../assets/icons/italic-fill.svg";
+import { ReactComponent as QuoteIcon } from "../../assets/icons/quote-fill.svg";
 
 import { Card } from "../cards";
 
@@ -40,72 +40,74 @@ export const FloatingMenu = (props: FloatingMenuArgs) => {
   if (!editor) {
     return null;
   }
-  
-  return (
-    <BubbleMenu {...props} editor={editor}>
-      <MenuContainer isFloating>
-        <StyledIconButton
-          size={"small"}
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 1 }).run()
-          }
-          isBasic={!editor.isActive("heading", { level: 1 })}
-          isPrimary={editor.isActive("heading", { level: 1 })}
-          isPill={false}
-        >
-          <H1Icon key="ug-h1-button-bubble-menu" />
-        </StyledIconButton>
-        <StyledIconButton
-          size={"small"}
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 2 }).run()
-          }
-          isBasic={!editor.isActive("heading", { level: 2 })}
-          isPrimary={editor.isActive("heading", { level: 2 })}
-          isPill={false}
-        >
-          <H2Icon key="ug-h2-button-bubble-menu" />
-        </StyledIconButton>
-        <StyledIconButton
-          size={"small"}
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 3 }).run()
-          }
-          isBasic={!editor.isActive("heading", { level: 3 })}
-          isPrimary={editor.isActive("heading", { level: 3 })}
-          isPill={false}
-        >
-          <H3Icon key="ug-h3-button-bubble-menu" />
-        </StyledIconButton>
 
-        <StyledIconButton
-          size={"small"}
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          isBasic={!editor.isActive("bold")}
-          isPrimary={editor.isActive("bold")}
-          isPill={false}
-        >
-          <BoldIcon key="ug-bold-button-bubble-menu" />
-        </StyledIconButton>
-        <StyledIconButton
-          size={"small"}
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          isBasic={!editor.isActive("italic")}
-          isPrimary={editor.isActive("italic")}
-          isPill={false}
-        >
-          <ItalicIcon key="ug-italic-button-bubble-menu" />
-        </StyledIconButton>
-        <StyledIconButton
-          size={"small"}
-          onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          isBasic={!editor.isActive("blockquote")}
-          isPrimary={editor.isActive("blockquote")}
-          isPill={false}
-        >
-          <QuoteIcon key="ug-quote-button-bubble-menu" />
-        </StyledIconButton>
-      </MenuContainer>
-    </BubbleMenu>
+  return (
+    <div>
+      <BubbleMenu {...props} editor={editor}>
+        <MenuContainer isFloating>
+          <StyledIconButton
+            size={"small"}
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+            isBasic={!editor.isActive("heading", { level: 1 })}
+            isPrimary={editor.isActive("heading", { level: 1 })}
+            isPill={false}
+          >
+            <H1Icon key="ug-h1-button-bubble-menu" />
+          </StyledIconButton>
+          <StyledIconButton
+            size={"small"}
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 2 }).run()
+            }
+            isBasic={!editor.isActive("heading", { level: 2 })}
+            isPrimary={editor.isActive("heading", { level: 2 })}
+            isPill={false}
+          >
+            <H2Icon key="ug-h2-button-bubble-menu" />
+          </StyledIconButton>
+          <StyledIconButton
+            size={"small"}
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 3 }).run()
+            }
+            isBasic={!editor.isActive("heading", { level: 3 })}
+            isPrimary={editor.isActive("heading", { level: 3 })}
+            isPill={false}
+          >
+            <H3Icon key="ug-h3-button-bubble-menu" />
+          </StyledIconButton>
+
+          <StyledIconButton
+            size={"small"}
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            isBasic={!editor.isActive("bold")}
+            isPrimary={editor.isActive("bold")}
+            isPill={false}
+          >
+            <BoldIcon key="ug-bold-button-bubble-menu" />
+          </StyledIconButton>
+          <StyledIconButton
+            size={"small"}
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            isBasic={!editor.isActive("italic")}
+            isPrimary={editor.isActive("italic")}
+            isPill={false}
+          >
+            <ItalicIcon key="ug-italic-button-bubble-menu" />
+          </StyledIconButton>
+          <StyledIconButton
+            size={"small"}
+            onClick={() => editor.chain().focus().toggleBlockquote().run()}
+            isBasic={!editor.isActive("blockquote")}
+            isPrimary={editor.isActive("blockquote")}
+            isPill={false}
+          >
+            <QuoteIcon key="ug-quote-button-bubble-menu" />
+          </StyledIconButton>
+        </MenuContainer>
+      </BubbleMenu>
+    </div>
   );
 };
