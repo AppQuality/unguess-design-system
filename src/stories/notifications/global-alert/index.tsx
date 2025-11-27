81a7c3ebc6d0810329081f3e55f0ff5ee7450f7d
iacopolea <iacopo.lea@gmail.com>
2025-11-27 15:50:56 +0100

    fix: GlobalAlert styles

--- src/stories/notifications/global-alert/index.tsx
+++ src/stories/notifications/global-alert/index.tsx
@@ -25,9 +25,7 @@ export interface GlobalAlertProps extends Omit<IGlobalAlertProps, "type"> {
 const StyledGlobalAlert = styled(ZendeskGlobalAlert)<GlobalAlertProps>`
   // general
   align-items: center;
-  [data-garden-id="notifications.global-alert.icon"] {
-    margin-top: -2px;
-  }
+
   .global-alert-icon {
     flex-shrink: 0;
     margin-top: -2px;
@@ -37,100 +35,6 @@ const StyledGlobalAlert = styled(ZendeskGlobalAlert)<GlobalAlertProps>`
   }
   .global-alert-cta {
     flex-shrink: 0;
-  }
-
-  // looks like the following rules get overwritten by garden if we use props.type directly instead of alerttype
-  // or even in file component.ts of the theme
-
-  // Primary
-  &[alerttype="primary"] {
-    background-color: ${({ theme }) => getColor(theme.colors.primaryHue, 100)};
-    color: ${({ theme }) => getColor(theme.colors.primaryHue, 700)};
-    box-shadow: 0 1px 1px
-      ${({ theme }) => getColor(theme.colors.primaryHue, 300)};
-
-    [data-garden-id="notifications.global-alert.content"] {
-      color: ${({ theme }) => getColor(theme.colors.primaryHue, 700)};
-    }
-    [data-garden-id="notifications.global-alert.title"] {
-      color: ${({ theme }) => getColor(theme.colors.primaryHue, 800)};
-    }
-    [data-garden-id="buttons.anchor"] {
-      color: ${({ theme }) => getColor(theme.colors.primaryHue, 600)};
-    }
-    .global-alert-icon {
-      color: ${({ theme }) => getColor(theme.colors.primaryHue, 700)};
-    }
-  }
-
-  // Accent
-  &[alerttype="accent"] {
-    background-color: ${({ theme }) => theme.palette.blue[100]};
-    color: ${({ theme }) => getColor(theme.colors.primaryHue, 700)};
-    box-shadow: 0 1px 1px
-      ${({ theme }) => getColor(theme.colors.accentHue, 600)};
-
-    [data-garden-id="notifications.global-alert.content"] {
-      color: ${({ theme }) => getColor(theme.colors.accentHue, 700)};
-    }
-    [data-garden-id="notifications.global-alert.title"] {
-      color: ${({ theme }) => getColor(theme.colors.accentHue, 800)};
-    }
-    [data-garden-id="buttons.anchor"] {
-      color: ${({ theme }) => getColor(theme.colors.accentHue, 700)};
-    }
-    .global-alert-icon {
-      color: ${({ theme }) => getColor(theme.colors.accentHue, 700)};
-    }
-  }
-
-  // Info
-  &[alerttype="info"] {
-    background-color: ${({ theme }) => getColor(theme.colors.infoHue, 100)};
-    color: ${({ theme }) => getColor(theme.colors.primaryHue, 700)};
-
-    [data-garden-id="notifications.global-alert.content"] {
-      color: ${({ theme }) => getColor(theme.colors.infoHue, 700)};
-    }
-    [data-garden-id="notifications.global-alert.title"] {
-      color: ${({ theme }) => getColor(theme.colors.infoHue, 800)};
-    }
-    [data-garden-id="buttons.anchor"] {
-      color: ${({ theme }) => getColor(theme.colors.infoHue, 700)};
-    }
-    [data-garden-id="notifications.global-alert.icon"] {
-      color: ${({ theme }) => getColor(theme.colors.infoHue, 700)};
-    }
-  }
-
-  // Error
-  &[alerttype="error"] {
-    .global-alert-cta {
-      background-color: ${({ theme }) => getColor(theme.colors.dangerHue, 800)};
-    }
-  }
-
-  // Warning
-  &[alerttype="warning"] {
-    background-color: ${({ theme }) => getColor(theme.colors.warningHue, 300)};
-    box-shadow: 0 1px 1px
-      ${({ theme }) => getColor(theme.colors.warningHue, 600)};
-    .global-alert-cta {
-      background-color: ${({ theme }) =>
-        getColor(theme.colors.warningHue, 800)};
-    }
-  }
-
-  // Success
-  &[alerttype="success"] {
-    background-color: ${({ theme }) => getColor(theme.colors.successHue, 700)};
-    box-shadow: 0 1px 1px
-      ${({ theme }) => getColor(theme.colors.successHue, 700)};
-    color: ${({ theme }) => getColor(theme.colors.successHue, 50)};
-    .global-alert-cta {
-      background-color: ${({ theme }) =>
-        getColor(theme.colors.successHue, 800)};
-    }
   }
 `;
 
