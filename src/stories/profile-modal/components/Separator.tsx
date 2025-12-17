ca4d184f5a7c375edd1d00b531e09dac676a678b
Davide Bizzi <davide.bizzi@unguess.io>
2025-12-17 17:36:48 +0100

    fix: Remove redundant border-top style from Separator component

--- src/stories/profile-modal/components/Separator.tsx
+++ src/stories/profile-modal/components/Separator.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Separator = styled.hr`
   display: block;
   margin: 4px 0px;
+  border-top: none;
   border-bottom: 1px solid ${(props) => props.theme.palette.grey[300]};
 `;
 
