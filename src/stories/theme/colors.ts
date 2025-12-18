ac507ac1f65a8a526c91fca914b1b1ff76702d99
Davide Bizzi <davide.bizzi@unguess.io>
2025-12-18 11:33:46 +0100

    feat: Extend ThemeColors with additional light variable properties

--- src/stories/theme/colors.ts
+++ src/stories/theme/colors.ts
@@ -7,6 +7,38 @@ type ThemeColors = typeof DEFAULT_THEME.colors & {
 export const colors: ThemeColors = {
   ...DEFAULT_THEME.colors, // we are overwriting this but want to keep the original colors as reference
   base: "light",
+  variables: {
+    ...DEFAULT_THEME.colors.variables,
+    light: {
+      ...DEFAULT_THEME.colors.variables.light,
+      border: {
+        default: "neutralHue.300",
+        emphasis: "neutralHue.600",
+        subtle: "neutralHue.200",
+        success: "successHue.300",
+        warning: "warningHue.300",
+        danger: "dangerHue.300",
+        primaryEmphasis: "primaryHue.600",
+        successEmphasis: "successHue.600",
+        warningEmphasis: "warningHue.600",
+        dangerEmphasis: "dangerHue.600",
+        disabled: "neutralHue.300",
+      },
+      foreground: {
+        default: "#2f3941",
+        subtle: "neutralHue.700",
+        onEmphasis: "palette.white",
+        primary: "primaryHue.600",
+        success: "successHue.600",
+        warning: "warningHue.600",
+        danger: "dangerHue.600",
+        successEmphasis: "successHue.900",
+        warningEmphasis: "warningHue.900",
+        dangerEmphasis: "dangerHue.900",
+        disabled: "neutralHue.600",
+      },
+    },
+  },
   primaryHue: "blue",
   dangerHue: "red",
   warningHue: "yellow",
