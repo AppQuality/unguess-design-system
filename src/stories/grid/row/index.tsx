4585f2019b0bfce06881eb5cf4d8fc7a86e25b6c
Davide Bizzi <davide.bizzi@unguess.io>
2025-12-18 14:58:09 +0100

    fix: Update imports to use Grid component from ZendeskGrid for consistency

--- src/stories/grid/row/index.tsx
+++ src/stories/grid/row/index.tsx
@@ -1,6 +1,6 @@
-import { Row as ZendeskRow } from "@zendeskgarden/react-grid";
+import { Grid as ZendeskGrid } from "@zendeskgarden/react-grid";
 import { RowArgs } from "./_types";
 
-const Row = (props: RowArgs) => <ZendeskRow {...props} />;
+const Row = (props: RowArgs) => <ZendeskGrid.Row {...props} />;
 
 export { Row };
