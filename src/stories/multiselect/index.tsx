425c2bdd2b06ac6a3ed4ac14a52db028394be733
Davide Bizzi <davide.bizzi@unguess.io>
2026-01-12 13:11:33 +0100

    fix: Update import path for Separator component to maintain consistency

--- src/stories/multiselect/index.tsx
+++ src/stories/multiselect/index.tsx
@@ -2,7 +2,7 @@ import { Field } from "@zendeskgarden/react-dropdowns";
 import { useEffect, useState } from "react";
 import { ReactComponent as GridAddIcon } from "../../assets/icons/plus.svg";
 import { Combobox } from "../combobox";
-import { Separator } from "../dropdowns/menu";
+import { Separator } from "../profile-modal/components/Separator";
 import { SelectOption } from "../selectOption";
 import { theme } from "../theme";
 import { MultiSelectProps } from "./_types";
